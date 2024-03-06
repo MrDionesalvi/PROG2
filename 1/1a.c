13d784c4a04d5ffe9922c8e3b455256da13d04b6
MrDionesalvi <37485674+MrDionesalvi@users.noreply.github.com>
2024-03-06 14:54:28 +0100

    Update 1a.c

--- 1/1a.c
+++ 1/1a.c
@@ -1,45 +1,52 @@
-/* scrivere qui la vostra suite di test */
-
-/* ad esempio: */
-
-
-tests[0].s = "\n3";   //check7
+// Test numero 1-3-7
+tests[0].s = "\n3";   
 tests[0].b = 10;
 tests[0].res = 1;
 tests[0].num = 3;
 
-tests[1].s = "0";   //check2
+// Test numero 2
+tests[1].s = "0";   
 tests[1].b = 1;
 tests[1].res = 0;
 tests[1].num = 0;
 
-tests[2].s = "15NF";    //check10
+
+// Test numero 6-10
+tests[2].s = "15NF";    
 tests[2].b = 36;
 tests[2].res = 1;
 tests[2].num = 53979;
 
-tests[3].s = "  +2";    //check 3/4/5
+
+
+// Test numero 4-5
+tests[3].s = "  +2";    
 tests[3].b = 10;
 tests[3].res = 1;
 tests[3].num = 2;
 
-tests[4].s = "A";   // CHECK(7)
+
+// Test numero 7
+tests[4].s = "A";
 tests[4].b = 11;
 tests[4].res = 1;
 tests[4].num = 10;
 
-tests[5].s = "23";     //check(12)
+
+// Test numero 12
+tests[5].s = "23";     
 tests[5].b = 3;
 tests[5].res = 1;
 tests[5].num = 2;
 
-tests[6].s = "10"; //check1q
+// Test numero 10
+tests[6].s = "10";
 tests[6].b = 16;
 tests[6].res = 1;
 tests[6].num = 16;
 
-tests[7].s = "         "; //check8
+// Test numero 8
+tests[7].s = " "; 
 tests[7].b = 16;
 tests[7].res = 0;
 tests[7].num = 0;
-
